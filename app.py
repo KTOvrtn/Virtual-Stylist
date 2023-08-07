54c9f53e116018a87152bb6126afe33e7f4bcbf6
KTOvrtn <124303748+KTOvrtn@users.noreply.github.com>
2023-08-07 21:56:38 +0100

    Navbar progress

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+from flask import Flask, render_template, url_for
+
+app = Flask('Stylista')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
