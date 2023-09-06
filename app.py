abd9b65dac76311cb0422f1806377a5eef3dcac0
KTOvrtn <124303748+KTOvrtn@users.noreply.github.com>
2023-09-06 22:56:21 +0100

    finished the login system

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, flash, session
+from flask import Flask, render_template, url_for, request, flash, session, redirect
 from databases import *
 
 app = Flask('Stylista')
@@ -17,12 +17,31 @@ def quiz():
         answer4 = request.form["color"]
         answer5 = request.form["colour"]
         answer6 = request.form["comfort"]
-        insert_answer(answer1, answer2, answer3, answer4, answer5, answer6)
+        add_quiz_results(session["username"], answer1, answer2, answer3, answer4, answer5, answer6)
         print(answer1, answer2, answer3, answer4, answer5, answer6)
         flash('Your answers have been submitted!')
         return render_template('quiz.html')
     else:
-        return render_template('quiz.html')
+        if "username" in session:
+            return render_template('quiz.html')
+        else:
+            flash("You need to login first!")
+            return redirect(url_for('login'))
+    
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if check_login(username, password) == False:
+            flash("Your credentials are wrong, please try again!")
+            return render_template("login.html")
+        else:
+            flash("Successful login!")
+            session["username"] = username
+            return render_template('index.html')
+    else:
+        return render_template("login.html")
 
 
 if __name__ == '__main__':
--- databases.py
+++ databases.py
@@ -1,18 +1,74 @@
 import sqlite3
+import hashlib
 
-def create_table():
-    conn = sqlite3.connect('quiz_answers.db')
-    c = conn.cursor()
-    c.execute('''CREATE TABLE if not exists answers
-                    (answer1 text, answer2 text, answer3 text, answer4 text, answer5 text, answer6 text)''')
-    conn.commit()
-    conn.close()
 
-def insert_answer(answer1, answer2, answer3, answer4, answer5, answer6):
-    conn = sqlite3.connect('quiz_answers.db')
-    c = conn.cursor()
-    c.execute('''INSERT INTO answers VALUES (?, ?, ?, ?, ?, ?)''', (answer1, answer2, answer3, answer4, answer5, answer6))
-    conn.commit()
-    conn.close()
-    
-create_table()
+def create_user_table():
+    connection = sqlite3.connect("database.db")
+    cursor = connection.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY AUTOINCREMENT,username TEXT,email TEXT,password TEXT)")
+    connection.commit()
+
+def add_user(username, email, password):
+    connection = sqlite3.connect("database.db")
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO users (username, email, password) VALUES (?, ?, ?)",(username, email, hash_password(password)))
+    connection.commit()
+
+def hash_password(password):
+    hashed_password = hashlib.sha512(password.encode()).hexdigest() #https://docs.python.org/3/library/hashlib.html
+    return hashed_password
+
+def check_username(username):
+    connection = sqlite3.connect("database.db")
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM users WHERE username=?",(username,))
+    users_the_same = cursor.fetchall()
+    if users_the_same:
+        return True
+    else:
+        return False
+
+def check_email(email):
+    connection = sqlite3.connect("database.db")
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM users WHERE email=?",(email,))
+    email_the_same = cursor.fetchall()
+    if email_the_same:
+        return True
+    else:
+        return False
+
+def check_login(username, password):
+    connection = sqlite3.connect("database.db")
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM users WHERE username=? AND password=?",(username, hash_password(password)))
+    login_the_same = cursor.fetchall()
+    if login_the_same:
+        return username
+    else:
+        return False
+
+def create_quiz_table():
+    connection = sqlite3.connect("database.db")
+    cursor = connection.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS quiz (username TEXT PRIMARY KEY , answer1 TEXT, answer2 TEXT, answer3 TEXT, answer4 TEXT, answer5 TEXT, answer6 TEXT)")
+    connection.commit()
+
+def add_quiz_results(username, answer1, answer2, answer3, answer4, answer5, answer6):
+    connection = sqlite3.connect("database.db")
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO quiz (username, answer1, answer2, answer3, answer4, answer5, answer6) VALUES (?, ?, ?, ?, ?, ?, ?)",(username, answer1, answer2, answer3, answer4, answer5, answer6))
+    connection.commit()
+
+def get_quiz_results(username):
+    connection = sqlite3.connect("database.db")
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM quiz WHERE username=?",(username,))
+    quiz_results = cursor.fetchall()
+    if quiz_results:
+        return quiz_results
+    else:
+        return False
+
+create_quiz_table()
+create_user_table()
